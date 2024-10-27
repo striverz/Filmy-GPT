c1e44c02a3ecbc3c98d5649058540be2fe1c3233
striverz <striverz100x@gmail.com>
2024-10-27 23:19:25 +0530

    added some styling to the videotitle and play component

--- src/components/VideoPlay/VideoPlay.jsx
+++ src/components/VideoPlay/VideoPlay.jsx
@@ -21,7 +21,7 @@ const VideoPlay = ({movieId}) => {
       <iframe
       
       className="iframe-style"
-      src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0&showinfo=0&rel=0&modestbranding=0`}
+      src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0&showinfo=0&rel=0&modestbranding=1`}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       
